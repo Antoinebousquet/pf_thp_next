57a489c50932c256da3445d5512d6df56baaf487
Antoinebousquet <moodlinkproject@gmail.com>
2020-01-08 18:32:54 +0100

    add agenda and notes to patient dashboard + date into dashbord

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,11 +2,11 @@ class AvatarsController < ApplicationController
 	
 
 	def create
-	  if 
+	  if current_practionner === true
 	    @practionner = Practionner.find(current_practionner.id)
 	    @practionner.avatar.attach(params[:avatar])
 	    redirect_to(practionners_path(@practionner))
-	  else current_patient === true
+	  else 
 	    @patient = Patient.find(current_patient.id)
 	    @patient.avatar.attach(params[:avatar])
 	    redirect_to(patients_path(@patient))
